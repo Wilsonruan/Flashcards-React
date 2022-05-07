4157b11d25e3f079683623db7f7adfb6cd3e964e
Wilson <wruan@tvo.org>
2022-05-07 09:08:32 -0400

    center app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './css/Card.scss';
 import Carousel from './components/Carousel';
 
 function App() {
-  let Flashcards = require('./widgets/flashcards1.json');
+  let Flashcards = require('./widgets/flashcards.json');
   return (
     <main className="flashcard-main-container">
+        <h1>Flashcards React App</h1> 
         <Carousel
           instruction={Flashcards.instruction}
           items={Flashcards.data} />
